8792d178b9e1cff2999efe5cd442529b034f3f8d
Edgaras Ivaskevicius <edgaras@ivaskevicius.com>
2014-08-19 20:06:32 -0400

    Improvements to 'try it' functionality

--- js/parser-demo.js
+++ js/parser-demo.js
@@ -5,7 +5,7 @@ $(function() {
         e.preventDefault();
     });
 
-    $('#btn-parse').on('click', function() {
+    $('#lnk-parse').on('click', function() {
         var address = $('#txt-address').val();
 
         if (address == '') {
@@ -13,24 +13,47 @@ $(function() {
             $('#txt-address').val(address);
         }
 
+        $('#tbl-address-elements > tbody > tr > td:nth-child(2)').text('');
+        $('#formatted-line-1').text('');
+        $('#formatted-line-2').text('');
+        $('#raw-response').text('');
+
         $.post('http://api.addresslabs.com/v1/parsed-address', {
             address: address
         }).done(function(data) {
-            alert('Output JSON:\n\n' + JSON.stringify(data, null, 4));
+            $('.parsed-address-data').show();
+            $('.parsed-address-error').hide();
+            $('#formatted-line-1').text(data.delivery.address_line);
+            $('#formatted-line-2').text(data.delivery.last_line);
+            $('#raw-response').text(JSON.stringify(data, null, 4));
+            setAddressElements(data);
         }).fail(function() {
-            alert('Oops! Something went wrong.. Make sure you entered a valid address and try again.')
+            $('.parsed-address-data').hide();
+            $('.parsed-address-error').show();
         });
     });
 });
 
 function getRandomAddress() {
     var addresses = [
-        "10730 Main Street Fairfax Virginia 22030",
-        "589 garfield street s tupelo ms 38801-2109",
-        "5830 bright freeway south confederate ok 74533",
-        "2082 Shady Lane Unit 5 Davidson Missouri 64728",
+        "10 South Main Street Unit 3 Fairfax Virginia 22030",
+        "5897 garfield street s tupelo ms 38801-2109",
+        "58 bright freeway south apartment 5 confederate ok 74533",
+        "12 Shady Lane Suite 5 Davidson Missouri 64728",
         "Golden Wagon Cove at Main St Ducktown Missouri 64954-7706"
     ];
 
     return addresses[Math.floor(Math.random() * addresses.length)];
+}
+
+function setAddressElements(data) {
+    for (var element in data) {
+        if (data.hasOwnProperty(element)) {
+            if (typeof(data[element]) === 'object') {
+                setAddressElements(data[element]);
+            } else {
+                $('#' + element).text(data[element]);
+            }
+        }
+    }
 }
